acd3e5d02a8b08995489b14ca227db9a0e68c09e
Sergei Moskalyov <sv.moskalyov@gmail.com>
2022-10-09 11:50:10 +0300

    replaces header js

--- src/js/header.js
+++ src/js/header.js
@@ -1,18 +1,21 @@
 const burger = document?.querySelector('[data-burger]');
+const box = document?.querySelector('[data-box]');
 const nav = document?.querySelector('[data-nav]');
 const body = document.body;
 const navItems = nav?.querySelectorAll('a');
 
 burger?.addEventListener('click', () => {
   body.classList.toggle('stop-scroll');
-  burger?.classList.toggle('burger--active');
-  nav?.classList.toggle('nav--visible');
+  burger?.classList.toggle('burger-active');
+  box?.classList.toggle('nav-visible');
+  nav?.classList.toggle('nav-visible');
 });
 
 navItems.forEach(el => {
   el.addEventListener('click', () => {
     body.classList.remove('stop-scroll');
-    burger?.classList.remove('burger--active');
-    nav?.classList.remove('nav--visible');
+    burger?.classList.remove('burger-active');
+    box?.classList.remove('nav-visible');
+    nav?.classList.remove('nav-visible');
   });
 });
