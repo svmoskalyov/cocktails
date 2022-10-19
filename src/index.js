1618ab8792322c4a755daa2d034e9f3145af8fc0
Sergei Moskalyov <sv.moskalyov@gmail.com>
2022-10-19 21:11:48 +0300

    Update index.js

--- src/index.js
+++ src/index.js
@@ -1,5 +1,19 @@
 import './js/bodyAutoTop';
 import './js/burger';
 import './js/themeSwitch';
+import './js/heroList';
+
+import './js/coctails';
+import './js/favCoctails';
+import './js/favIngridients';
+import './js/getRandomCoctails';
+
 import './js/searchByInput';
-import './js/header';
+import './js/searchByLetter';
+
+import './js/modalCoctails';
+import './js/modalComponents';
+
+import './js/service/apiData';
+import './js/service/firebase';
+import './js/service/firebaseConfig';
